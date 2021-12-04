9f383edecee843cfe274df3e8ccfb131e597d89b
j.espino <j.espino@alumnos.upm.es>
2021-12-04 20:52:48 +0100

    indexOf nuevo

--- src/test/java/com/practica/cajanegra/indexOfTest.java
+++ src/test/java/com/practica/cajanegra/indexOfTest.java
@@ -1,22 +1,22 @@
 package com.practica.cajanegra;
 
 import com.cajanegra.SingleLinkedListImpl;
+import org.junit.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class indexOfTest {
     @ParameterizedTest
     @CsvSource({
-            "@",
+
             "A",
             "B",
             "M",
             "Y",
-            "Z",
-            "[",
+            "Z"
+
     })
     void testListaVacia(String element) {
         SingleLinkedListImpl<String> lista = new SingleLinkedListImpl<>();
@@ -24,34 +24,55 @@ public class indexOfTest {
     }
     @ParameterizedTest
     @CsvSource({
-            "@,0",
+
             "A,1",
             "B,2",
             "M,3",
             "Y,4",
             "Z,5",
-            "[,0",
+
     })
-    void testListaOK(String element,int p) {
+    void testListaCorrespondida(String element,int p) {
         SingleLinkedListImpl<String> lista = new SingleLinkedListImpl<>("A","B","M","Y","Z");
-        if(p == 0)
-        assertThrows(java.util.NoSuchElementException.class, () -> lista.indexOf(element));
-        else
-            assertEquals(lista.getAtPos(p), element);
+        assertEquals(lista.indexOf(element), p);
     }
     @ParameterizedTest
     @CsvSource({
-            "@",
+
             "A",
             "B",
             "M",
             "Y",
             "Z",
-            "[",
+
     })
-    void testListaMal(String element) {
+    void testListaNoCorrespondida(String element) {
         SingleLinkedListImpl<String> lista = new SingleLinkedListImpl<>("E","F","G","H","I");
         assertThrows(java.util.NoSuchElementException.class, () -> lista.indexOf(element));
+
+    }
+    @ParameterizedTest
+    @CsvSource({
+
+            "@,1",
+            "[,2",
+
+    })
+    void testValoresFueraRango(String element, int p){
+        SingleLinkedListImpl<String> lista = new SingleLinkedListImpl<>("@","[");
+        assertEquals(lista.indexOf(element), p);
+
+    }
+    @Test
+    void testNullListaVacia(){
+        SingleLinkedListImpl<String> lista = new SingleLinkedListImpl<>();
+        assertThrows(java.util.NoSuchElementException.class, () -> lista.indexOf(null));
+
+    }
+    @Test
+    void testNullListaLlena(){
+        SingleLinkedListImpl<String> lista = new SingleLinkedListImpl<>("A","B","M","Y","Z");
+        assertThrows(java.util.NoSuchElementException.class, () -> lista.indexOf(null));
 
     }
 }
